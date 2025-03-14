b2267db5848bfa22b6a89cd90b1d155a69863311
mithila <mwickramarathna@mitrai.com>
2025-03-14 10:03:21 +0530

    completed Update Beneficairy TestCase

--- src/test/java/api/methods/updateBeneficiaries.java
+++ src/test/java/api/methods/updateBeneficiaries.java
@@ -1,14 +1,22 @@
 package api.methods;
 
+import api.utils.ConstantApiUtils;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static api.utils.ConstantApiUtils.*;
 import static utils.CommonUtils.USER_DIR;
+import static utils.DataStoreReadWriteApi.getAPIDetails;
 
 public class updateBeneficiaries extends baseMethod {
     File jsonBody = new File(POST_BODY);
@@ -18,7 +26,9 @@ public class updateBeneficiaries extends baseMethod {
     private static final String JSON_PATH = USER_DIR + GET_UPDATE_BENEFICIARIES_RESPONSE;
     private static final String POST_BODY = USER_DIR + GET_UPDATE_BENEFICIARIES_BODY;
 
-
+    private FileReader file;
+    JSONObject jsonObject;
+    JSONParser jsonParser = new JSONParser();
     public void authorisedWithInvalidToken() {
         headersMap.put(TXT_AUTHORIZATION, TXT_AUTHORIZATION_INVALID_VAL);
     }
@@ -29,7 +39,29 @@ public class updateBeneficiaries extends baseMethod {
         headersMap.put(TXT_CONTENT_TYPE, TXT_APPLICATION_JSON);
         headersMap.put(TXT_X_REQUEST_ID, TXT_X_REQUEST_ID_VALUE_HUNDRED_AND_TWENTY_TWO);
     }
+    public void setPayloadWithValidBeneId() throws IOException, ParseException {
+        jsonBody = new File(ConstantApiUtils.PATH_TO_PAYLOAD_FOLDER.concat("updateBeneficiariesBody.json"));
+        file = new FileReader(ConstantApiUtils.PATH_TO_PAYLOAD_FOLDER.concat("updateBeneficiariesBody.json"));
+        jsonObject = (JSONObject) jsonParser.parse(file);
+        JSONObject updateBeneficiaries = (JSONObject) jsonObject.get("updateBeneficiaries");
+        updateBeneficiaries.put("beneficiaryID",  getAPIDetails("beneficiaryID"));
+        FileWriter writer = new FileWriter(jsonBody, false); //overwrites the content of file
+        writer.write(jsonObject.toString());
+        writer.close();
+    }
 
+    public void setResponceWithValidBeneId() throws IOException, ParseException {
+        jsonBody = new File(JSON_PATH);
+        file = new FileReader(JSON_PATH);
+        jsonObject = (JSONObject) jsonParser.parse(file);
+
+        JSONObject deleteBeneficiariesResponse = (JSONObject) jsonObject.get("updateBeneficiariesResponse");
+        JSONObject responseHeader = (JSONObject) deleteBeneficiariesResponse.get("responseHeader");
+        responseHeader.put("statusDescription","BeneficiaryID :: "+ getAPIDetails("beneficiaryID") + " record updated.");
+        FileWriter writer = new FileWriter(jsonBody, false);
+        writer.write(jsonObject.toString());
+        writer.close();
+    }
 
     public void invokeUpdateBeneficiariesApi() {
         setHeaders();
