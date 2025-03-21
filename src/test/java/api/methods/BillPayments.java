b5895686cf9e4f3453648cba7b5ad01556c0385e
mithila <mwickramarathna@mitrai.com>
2025-03-21 10:54:19 +0530

    BillPayments File Created

---
+++ src/test/java/api/methods/BillPayments.java
@@ -0,0 +1,35 @@
+package api.methods;
+
+import api.utils.baseRequest;
+import api.utils.helper;
+import api.utils.validatePayload;
+import api.utils.validateResponse;
+
+//getAccountsByTranType
+//billers
+//validate Payment
+//make BillPayment
+//saveBillTemplate
+//getPaymentsById
+public class BillPayments {
+    //-------------------BASE PATH SETUP ----------------------------
+    private final api.utils.baseRequest baseRequest = new baseRequest();
+    private final api.utils.validatePayload validatePayload = new validatePayload(baseRequest);
+    private final api.utils.validateResponse validateResponse = new validateResponse(baseRequest);
+    private final api.utils.helper helper = new helper(baseRequest);
+
+    //-----------------------------------------------  PAYLOAD SETUP  -----------------------------------------------
+
+    //-----------------------------------------------  RESPONSE SETUP  -----------------------------------------------
+
+    //-----------------------------------------------  Token SETUP  -----------------------------------------------
+
+    //-----------------------------------------------  Change BasePath  -----------------------------------------------
+    //-----------------------------------------------  SETUP PAYLOAD  -----------------------------------------------
+    //-----------------------------------------------  INVOKE METHODS  -----------------------------------------------
+    //-----------------------------------------------  RESPONSE CODE VALIDATIONS  -----------------------------------------------
+    //-----------------------------------------------  HELPER METHODS  -----------------------------------------------
+    //-----------------------------------------------  RESPONSE VALIDATION SETUP  -----------------------------------------------
+    //-----------------------------------------------  RESPONSE VALIDATIONS  -----------------------------------------------
+
+}
